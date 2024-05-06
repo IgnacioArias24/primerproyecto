c96bcbea3e3689dd5faf9b160dfd29ac81c187df
IgnacioArias24 <168765837+IgnacioArias24@users.noreply.github.com>
2024-05-06 13:37:29 +0000

    clase 11

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-const obtenerDatosUsuario = require('./config')
+const {obtenerDatosUsuario} = require('./config')
 
 console.log(obtenerDatosUsuario)
+
+const { ['NOMBRESIT 0'] : nombre , ['DNI_usuario'] : dni , ['EDad_usuario_1'] : edad , ['IDD usuario(1)'] : id_usuario   } = obtenerDatosUsuario()
+
