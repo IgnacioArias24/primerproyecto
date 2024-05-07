5fada0b3b74fa8a9e70f8aab859932731000adc0
IgnacioArias24 <168765837+IgnacioArias24@users.noreply.github.com>
2024-05-07 15:10:46 +0000

    clase 12

--- tarea/server.js
+++ tarea/server.js
@@ -1,14 +1,21 @@
 const URL_API = 'https://jsonplaceholder.typicode.com/'
 
 const obtenerTodosLosUsuarios = async () => {
-    const response = await fetch(URL_API + '/users', {
+    const response = await fetch(URL_API + '/users');
+    const usuarios = await response.json();
+    return usuarios;
+}
+
+obtenerTodosLosUsuarios()
+
+const obtenerUsuarioPorId = async (id_usuario) => {
+    const response = await fetch('obtenerTodosLosUsuarios$(id)', {
         method: 'GET'
-    } )
-    if(response.status == 200){
-        console.log('todo joya pa')
+    })
+    if(response.id){
         const data = await response.json()
         console.log(data)
     }
 }
 
-obtenerTodosLosUsuarios()
+obtenerUsuarioPorId()
