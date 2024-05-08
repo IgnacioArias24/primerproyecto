e2da31d5893deb2f9f0cc05484b365bda0d4243d
IgnacioArias24 <168765837+IgnacioArias24@users.noreply.github.com>
2024-05-08 14:39:03 +0000

    clase 12

--- tarea/posts.js
+++ tarea/posts.js
@@ -1,9 +1,35 @@
-// const {URL_API} = require('./config');
+const {URL_API} = require('./config');
 
 const obtenerTodosLosPosteos = async () =>{
-    const response  = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const response  = await fetch(URL_API + '/posts');
     const posts = await response.json();
+    console.log(posts)
     return posts
 }
 
-console.log(obtenerTodosLosPosteos())
+
+const obtenerPosteoPorId = async (id_post) =>{
+    const response = await fetch(URL_API + `/posts/${id_post}`);
+    const post = await response.json();
+    console.log(post)
+    return post
+}
+
+const obtenerComentariosDePosteoPorId = async (id_post) =>{
+    const response = await fetch(URL_API + `/comments/${id_post}`);
+    const comentarios = await response.json();
+    console.log(comentarios)
+    return comentarios
+}
+
+
+const obtenerListadoDeEmailsDeLosComentarios = async (id_post) =>{
+   const comentarios = await obtenerComentariosDePosteoPorId(id_post)
+   const emails = comentarios.map(comentario => comentario.email)
+   console.log(emails)
+}
+
+// obtenerTodosLosPosteos()
+// obtenerPosteoPorId(5)
+// obtenerComentariosDePosteoPorId(4)
+obtenerListadoDeEmailsDeLosComentarios(1)
